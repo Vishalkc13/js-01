5185afa447734e8d4fd73c84b362ab4da00d7679
Vishal Kumar <139376956+Vishalkc13@users.noreply.github.com>
2023-09-13 17:41:32 +0000

    variable and datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 14434
+let accountEmail = "vidifn@gmail.com"
+var accountPassword = "9457"
+accountCity = "Muzaffarpur"
+let accountState;
+
+// accountId = 2 
+accountEmail = "vishal@gmail.com"
+accountPassword ="8475893"
+accountCity = "patna"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and funtional scope
+*/
+
+console.log(accountState);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+4) //we are using nodejs, not browser
+
+console.log(4 
+    + 4); //code readability should be high 
+
+//tc39.es documentaion ecmascript
+
+
+let name = "vishal" 
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => (let name;)
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object

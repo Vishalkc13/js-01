49d6e7b6f718e0d4b5a782e6b48c93b6914dadb5
Vishal Kumar <139376956+Vishalkc13@users.noreply.github.com>
2023-09-14 10:54:58 +0000

    conversation of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = 33
+// let score1 = "ef434"
+// let score1 = null
+// let score1 = undefined
+// let score1 = true
+let score1 = "Vishal kumar"
+
+
+// console.log(typeof score);
+
+// console.log(typeof (score1));
+
+let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+//"33" => 33
+//"33EH" => NAN
+//true => 1; false => 0;
+
+// let isLoggedIn =1
+// let isLoggedIn =0
+// let isLoggedIn = "vishal"
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//1=> true; 0 =>false;
+//"vishal"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

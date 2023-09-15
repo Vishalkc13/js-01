866048cd2d7fa9b30f0e0675ce852a9fba014782
Vishal Kumar <139376956+Vishalkc13@users.noreply.github.com>
2023-09-15 16:27:10 +0000

    maths and num

---
+++ 01_basic/07numMaths.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const aotherNumber = 3324.434
+
+// console.log(aotherNumber.toPrecision(4));
+// console.log(aotherNumber.toPrecision(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-In'));
+
+// maths----------------------------------------
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.584));
+// console.log(Math.round(4.354));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(5.8));
+
+// console.log(Math.min(3.43,5,8,4));
+// console.log(Math.max(45, 8.4, 86));
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.round((Math.random()*10)+1));
+// console.log(Math.ceil((Math.random()*10)+1));
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 20
+const max = 50
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);

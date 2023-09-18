b12356cd1bdb5ad149cd04026e04079d7d36c070
Vishal Kumar <139376956+Vishalkc13@users.noreply.github.com>
2023-09-18 05:02:47 +0000

    Date and Time

---
+++ 01_basic/08_dateAndtime.js
@@ -0,0 +1,53 @@
+// january 1 1970 
+
+// let mydate = new Date()
+// console.log(mydate);
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString()); //same date output
+// console.log(mydate.toJSON()); //same date output
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toUTCString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate); //Object
+
+// let myCreateDate = new Date(2023, 8, 18, 5, 3) 
+// let myCreateDate1 = Date(2023, 8, 18 )
+// let myCreateDate2 = new Date("2023-09-18")
+// let myCreateDate3 = new Date("09-18-2023")
+
+// console.log(myCreateDate2);
+// console.log(myCreateDate3);
+
+
+// console.log(myCreateDate);
+// console.log(myCreateDate1);
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toLocaleString());
+// console.log(myCreateDate1.toDateString());  //Not allwoed because mycreateDate1 is not funtion
+
+
+// let myTimestamp = Date.now()
+
+// console.log(myTimestamp);
+// console.log(myCreateDate3.getTime());
+
+// console.log(Math.floor(Date.now()/1000));//convert in second
+// console.log(myTimestamp);
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+
+// `${newDate.getDay()} and the time`
+
+// newDate.toLocaleDateString('default',{
+//     weekday: "long"
+//     // timeZone: ""
+// })
+

5cc96b6f28835a939105ff842cab4b37b0c98c10
Vishal Kumar <139376956+Vishalkc13@users.noreply.github.com>
2023-09-20 15:25:47 +0000

    funtion

---
+++ 03_basic/01_function.js
@@ -0,0 +1,58 @@
+// Function
+
+// console.log("k");
+// console.log("r");
+// console.log("i");
+// console.log("s");
+// console.log("h");
+// console.log("n");
+// console.log("a");
+
+
+
+function sayName() {
+    console.log("k");
+    console.log("r");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+    console.log("n");
+    console.log("a");
+
+}
+
+// sayName();
+
+
+// function addTwoNumbers(number1, number2){
+//    console.log( number1+number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1+number2
+    // return result
+
+    return number1+number2
+ }
+
+// addTwoNumbers()
+// addTwoNumbers(5, 7)
+// addTwoNumbers(5, "7")
+// addTwoNumbers(5, "a")
+// addTwoNumbers(5, null)
+
+// const result = addTwoNumbers(5, 9)
+// console.log("Result: ", result);
+
+
+
+function loginUserMessage(username){
+    if(username == undefined){
+
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("krishna"));
+console.log(loginUserMessage());
+

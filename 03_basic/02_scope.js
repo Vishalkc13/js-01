94110455db1805095be089a773c70abff9448cb7
Vishal Kumar <139376956+Vishalkc13@users.noreply.github.com>
2023-09-25 16:12:08 +0000

    scoape

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,83 @@
+// let a = 10
+// const b = 20
+var c = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+
+let a = 100
+const b = 200
+
+
+if(true){
+    let a = 10
+    const b = 20
+    // console.log(a);
+    // console.log(b);
+
+}
+
+// console.log(a);
+// console.log(b);
+
+
+
+function one(){
+    const username= "krishna"
+    function two (){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website); //not allowed website is not defined
+    // two()
+
+}
+
+// one()
+
+
+if(true){
+    const username = "krishna"
+    if(username == "krishna"){
+        const website = " youtube"
+        // console.log(username + website);
+
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// +++++++++++++++++++++++ interesying +++++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone (num){
+    return num + 1
+}
+
+// addone(5)
+
+
+// console.log(addtwo(5));
+const addtwo = function(num){
+    return num + 2
+}
+
+// console.log(addtwo(5));
+
+
